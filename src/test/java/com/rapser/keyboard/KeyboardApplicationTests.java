ea249ca07bda13ba787d679e0c30fb0f808f6f80
Miguel Angel Tomairo <matomairo@gmail.com>
2020-07-12 15:39:34 -0500

    first commit

---
+++ src/test/java/com/rapser/keyboard/KeyboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapser.keyboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
